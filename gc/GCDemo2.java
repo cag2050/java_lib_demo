ab1fc1b1f64ec791ee938087ae2061471ecb4e79
cag2050 <cag2050@163.com>
2017-09-24 21:22:21 +0800

    m

--- gc/GCDemo2.java
+++ gc/GCDemo2.java
@@ -4,6 +4,8 @@ public class GCDemo2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+//		这次 bytes 引用的对象所占的内存被回收了
+//		为对象赋 null 值并不是控制变量回收的最好方法，以恰当的变量作用域来控制变量回收时间才是最优雅的解决办法。另外，赋 null 值的操作在经过虚拟机 JIT 编译器优化后会被消除掉，经过 JIT 编译后，System.gc()执行时就可以正确地回收掉内存，而无需赋 null 值。
 		byte[] bytes = new byte[32 * 1024 * 1024];
 		bytes = null;
 		System.gc();
